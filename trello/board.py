4a7d960c3f820abf92d2881bdf0cf4951a1ca088
Akello (234.48:4) <43887608+DolicaAkelloEgwel@users.noreply.github.com>
2026-05-18 16:59:33 +0100

    board object

---
+++ trello/board.py
@@ -0,0 +1,44 @@
+import json
+import requests
+
+with open("trello.api", "r") as f:
+    API_KEY = f.read()
+
+with open("trello.token", "r") as f:
+    API_TOKEN = f.read()
+
+query = {
+  'key': API_KEY,
+  'token': API_TOKEN
+}
+
+headers = {
+  "Accept": "application/json"
+}  
+
+class Board:
+    def __init__(self, id: str):
+        self._id = id
+        self._lists_url = f"https://api.trello.com/1/boards/{self._id}/lists"
+
+    @property
+    def lists_url(self) -> str:
+        return self._lists_url
+
+    @property
+    def lists_as_json(self) -> dict:
+        response = requests.request(
+            "GET",
+            self.lists_url,
+            headers=headers,
+            params=query
+        )
+        return json.loads(response.text)
+
+    def get_list_id_by_name(self, name: str) -> str:
+        for val in self.lists_as_json:
+            if val["name"] == name:
+                return val["id"]
+
+    def add_card_to_board(self, title: str, content: str, board_id: str):
+        pass
